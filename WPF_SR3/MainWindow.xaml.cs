7503211be9e1eb0ca2808026ff321dbb69424bb0
Юлия Волкова <volktul@gmail.com>
2022-12-26 14:19:25 +0300

    Добавлены темы

--- WPF_SR3/MainWindow.xaml.cs
+++ WPF_SR3/MainWindow.xaml.cs
@@ -127,6 +127,14 @@ namespace WPF_SR3
                 File.WriteAllText(saveFileDialog.FileName, textBox.Text);
             }
         }
+
+        private void themes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Uri theme = new Uri(themes.SelectedIndex == 0 ? "Light.xaml" : "Dark.xaml",UriKind.Relative);
+            ResourceDictionary themeDict=Application.LoadComponent (theme) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(themeDict);
+        }
     }
 }
 
